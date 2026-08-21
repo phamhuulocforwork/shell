7e79f36004bc51233340c90a86f8995cd5a75094
Pham Huu Loc <phamhuulocforwork@gmail.com>
2026-08-21 10:30:39 +0700

    refactor(gif): personalization

--- plugin/src/Caelestia/Config/userpaths.hpp
+++ plugin/src/Caelestia/Config/userpaths.hpp
@@ -17,7 +17,7 @@ class UserPaths : public settings::ObjectNode {
         QString, wallpaperDir, QStandardPaths::writableLocation(QStandardPaths::PicturesLocation) + u"/Wallpapers"_s)
     CONFIG_GLOBAL_PROPERTY(
         QString, lyricsDir, QStandardPaths::writableLocation(QStandardPaths::MusicLocation) + u"/Lyrics/"_s)
-    CONFIG_PROPERTY(QString, sessionGif, u"root:/assets/kurukuru.gif"_s)
+    CONFIG_PROPERTY(QString, sessionGif, u"root:/assets/lizard.gif"_s)
     CONFIG_PROPERTY(QString, mediaGif, u"root:/assets/bongocat.gif"_s)
     CONFIG_PROPERTY(QString, noNotifsPic, u"root:/assets/dino.png"_s)
     CONFIG_PROPERTY(QString, lockNoNotifsPic, u"root:/assets/dino.png"_s)
